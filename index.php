63f6c6497b5f560ecf57ac55b3716020b3504cde
julian torossian <julian.torossian@outlook.com>
2020-10-20 18:33:47 -0300

    agrego archivos del template

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
   <!-- Bootstrap core CSS --><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
   <!-- Custom styles for this template -->
-  <link href="css/shop-homepage.css" rel="stylesheet">
+  <link href="css\style.css" rel="stylesheet">
 
 </head>
 
